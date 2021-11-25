9f1808838c6fe1577d4717fff9f9a87a3b5b304b
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-11-25 08:31:25 +0530

    Update Hsearch.ts

--- src/commands/Nsfw/Hsearch.ts
+++ src/commands/Nsfw/Hsearch.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
             return void M.reply(
                 `Sorry NSFW is not enabled`
             )
-        const buffer = await request.buffer(data.result[5].thumbnail.s).catch((e) => {
+        const buffer = await request.buffer(data.result.hasil.cover).catch((e) => {
             return void M.reply(e.message)
         })
         while (true) {
@@ -38,7 +38,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `🎀 *Title:* ${data.result[5].title}\n💮 *ID:* ${data.result[5].id}\n🌸 *Language:* ${data.result[5].language}`,
+                    `🎀 *Title:* ${data.result.hasil.title.default}\n💮 *ID:* ${data.result.hasil.id}\n🌸 *Language:* ${data.result.hasil.language}\n🀄 *parodies* :  ${data.result.hasil.details.parodies[0].name}\n${data.result.hasil.details.parodies[0].count}\n${data.result.hasil.details.parodies[0].link}\n 📃 *Artist* : ${data.result.hasil.details.artists[0].name}\n🗻 Favourites : ${data.result.hasil.favorites}\n 🎛*Uploaded* : ${data.result.hasil.uploadedAt}\n*${data.result.hasil.link}*   `,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
