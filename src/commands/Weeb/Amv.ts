9ea63114ab8b6e01a29b5c438f70db82e779264f
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-11-28 19:27:57 +0530

    Update Amv.ts

--- src/commands/Weeb/Amv.ts
+++ src/commands/Weeb/Amv.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
 
 run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         const { dat } =  await axios.get(`http://zekais-api.herokuapp.com/yts?query=amv&apikey=CnXf9Ojs`)
-        const i = Math.floor(Math.random() * data.result.length)
+        const i = Math.floor(Math.random() * dat.result.length)
                 const text = `${dat.result[i].url}`
                console.log(text)
        const { data } = await axios.get('http://zekais-api.herokuapp.com/ytmp4?url=${text}&apikey=CnXf9Ojs')
