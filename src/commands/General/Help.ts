c6e8fe4f3494eb0e8d359bb25c409e887e245ca0
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-11-30 09:41:08 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -42,7 +42,7 @@ export default class Command extends BaseCommand {
                 //`${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
             //)
 const id = 'abcd@s.whatsapp.net' 
-           const buttons = [ {buttonId: 'id1', buttonText: {displayText: '/mod'}, type: 1}, {buttonId: 'id2', buttonText: {displayText: '/cara'}, type: 1} ]
+           const buttons = ' {buttonId: 'id1', buttonText: {displayText: '/mod'}, type: 1}, {buttonId: 'id2', buttonText: {displayText: '/cara'}, type: 1} '
 const buttonMessage = { contentText: `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`, footerText: 'Cara', buttons: buttons, headerType: 1 } 
 return void this.client.sendMessage(M.from, buttonMessage, MessageType.buttonsMessage)
         }
