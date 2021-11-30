70befd5f29cf3ca62eb3bd0a3e69216bb887c6e9
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-11-30 09:17:21 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -2,7 +2,7 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
-const { MessageType} = require('@adiwajshing/baileys');
+import { MessageType, MessageOptions, Mimetype } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
