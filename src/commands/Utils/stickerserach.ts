0b3be1571f529f19bbc9e808aa30da52ddda55ec
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-12-02 23:06:43 +0530

    Update stickerserach.ts

--- src/commands/Utils/stickerserach.ts
+++ src/commands/Utils/stickerserach.ts
@@ -25,11 +25,11 @@ export default class Command extends BaseCommand {
         const cara = joined.trim()
         console.log(cara)
         
-        const { data } = await axios.get(`https://api.mojilala.com/v1/stickers/search?q=${cara}&api_key=dc6zaTOxFJmzC`)
+        const { data } = await axios.get(`http://zekais-api.herokuapp.com/stickerwa?query=${cara}&apikey=nE2Oyg9M`)
         
 if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
-        const i = Math.floor(Math.random() * data.data[0].id.images.fixed_height.length)
-const b = `${data.data[0].id.images.fixed_height[i].url}`
+        const i = Math.floor(Math.random() * data.result.length)
+const b = `${data.result[i]}`
 
         const sticker: any = await new Sticker(b, {
 			pack: "sticker",
