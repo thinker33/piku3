898038acb92a2fc29cb23bc1fb7686bd764bb0f6
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-12-03 10:22:55 +0530

    Update stickerserach.ts

--- src/commands/Utils/stickerserach.ts
+++ src/commands/Utils/stickerserach.ts
@@ -25,11 +25,11 @@ export default class Command extends BaseCommand {
         const cara = joined.trim()
         console.log(cara)
         
-        const { data } = await axios.get(`http://zekais-api.herokuapp.com/stickerwa?query=${cara}&apikey=nE2Oyg9M`)
+        const { data } = await axios.get(`https://g.tenor.com/v1/search?q=${cara}&key=LIVDSRZULELA&limit=8`)
         
 if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
-        const i = Math.floor(Math.random() * data.result.length)
-const b = `${data.result[i]}`
+        //const i = Math.floor(Math.random() * data.result.length)
+const b = `${data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.mp4?.url}`
 
         const sticker: any = await new Sticker(b, {
 			pack: "sticker",
