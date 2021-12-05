77260041f6be183888985e265af73a00724ace44
bpking7 <92138643+bpking7@users.noreply.github.com>
2021-12-06 02:57:54 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://media.tenor.com/videos/8a6408675b1f806290aaac4b939c5a98/mp4";
+			"https://tenor.com/boFGJ.gif";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Gommenasai, but I don't talk. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
