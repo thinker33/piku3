76715dfec6626e284214396f4844bb4eb80b377d
bpking7 <92138643+bpking7@users.noreply.github.com>
2021-12-10 12:40:43 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         MessageType.image,
                     undefined,
                     undefined,
-                    `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use !support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *!tada/chat (your text)* both are different ai chat bots\n*--->* If you want to add Star Chan in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
+                    `_*----🎀[Rule]🎀----*_\n\n❌*DONT DISTURB MODS*🚫\n*>>>*Don't send bad words in bot's dm*--->* Bot doesn't chat. *--->* If you want to add bot in your group the contact the owner by *,owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
