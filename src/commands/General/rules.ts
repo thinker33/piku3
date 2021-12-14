c600065c0377b9e17b81ef8c832bee5fdbf68fc8
bpking7 <92138643+bpking7@users.noreply.github.com>
2021-12-15 02:30:51 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -20,13 +20,13 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
-        return void M.reply( await request.buffer('https://i.pinimg.com/564x/96/eb/e1/96ebe1427aa8505cf56b110a620503a3.jpg'),
-        MessageType.image,
+        // fetch result of https://tenor.com/view/sappletoast-gif-21862218
+        return void M.reply( await request.buffer('https://c.tenor.com/U_dtJlIKJYEAAAAd/sappletoast.gif'),
+        MessageType.gif,
                     undefined,
                     undefined,
-                    `_*----🎀[Rule]🎀----*_\n\n❌*😇*🚫\n*>>>* use ,mods to know anything\n*--->*  bot doesn't talk. So don't try y
-To chat\n*--->* If you want to add bot in your group the contact the owner by *,mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
+                    `_*----⚠️[Rule]📋----*_\n\n📃Please Kindly follow the rules \n📢 use ,mods to know anything\n🔖  bot doesn't talk. So don't try
+To chat\n*📌 If you want to add bot in your group the contact the mods by *,mods* \n⚡ Dont use wrong command, 📎use the command given in the *help list* *(,help)* \n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n⚔️ Dont Dm the bot \n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
