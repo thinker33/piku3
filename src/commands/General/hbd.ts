3780df8347f6daf283088b222872f87f3aa83c57
bpking7 <92138643+bpking7@users.noreply.github.com>
2021-12-31 16:54:20 +0530

    Update hbd.ts

--- src/commands/General/hbd.ts
+++ src/commands/General/hbd.ts
@@ -17,6 +17,7 @@ export default class Command extends BaseCommand {
 			baseXp: 10,
 		});
 	}
+
 	run = async (M: ISimplifiedMessage): Promise<void> => {
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
@@ -33,6 +34,52 @@ export default class Command extends BaseCommand {
             pfp =
                 'https://wallpaperaccess.com/full/5304840.png'
         }
+        const exp = (await this.client.getUser(user)).Xp
+        let role: string;
+				if (exp < 500) {
+					role = "🌸 Citizen";
+				} else if (exp < 1000) {
+					role = "🔎 Cleric";
+				} else if (exp < 2000) {
+					role = "🔮 Wizard";
+				} else if (exp < 5000) {
+					role = "♦️ Mage";
+				} else if (exp < 10000) {
+					role = "🎯 Noble";
+				} else if (exp < 25000) {
+					role = "✨ Elite";
+				} else if (exp < 50000) {
+					role = "🔶️ Ace";
+				} else if (exp < 75000) {
+					role = "🌀 Hero";
+				} else if (exp < 100000) {
+					role = "💎 Supreme";
+				} else {
+					role = "❄️ Mystic";
+				}
+
+				let level: number;
+				if (exp < 500) {
+					level = 1;
+				} else if (exp < 1000) {
+					level = 2;
+				} else if (exp < 2000) {
+					level = 3;
+				} else if (exp < 5000) {
+					level = 4;
+				} else if (exp < 10000) {
+					level = 5;
+				} else if (exp < 25000) {
+					level = 6;
+				} else if (exp < 50000) {
+					level = 7;
+				} else if (exp < 75000) {
+					level = 8;
+				} else if (exp < 100000) {
+					level = 9;
+				} else {
+					level = 10;
+				}
         await M.reply(
             await request.buffer(
                 pfp ||
@@ -41,7 +88,11 @@ export default class Command extends BaseCommand {
             MessageType.image,
             undefined,
             undefined,
-            `🏮 *Username: ${username}*\n\n🎗️ *WISHING YOU A VERY VERY HAPPY BIRTHDAY*`
+            `🏮 *Username: ${username}*\n\n🎗️ *About: ${
+                (await this.client.getStatus(user)).status || 'None'
+            }*\n\n〽️ *Level: ${level}*\n\n⭐ *Exp: ${exp || 0}*\n\n💫 *Role: ${role}*\n\n👑 *Admin: ${
+                M.groupMetadata?.admins?.includes(user) || false
+            }*\n\n✖ *Ban: ${(await this.client.getUser(user)).ban || false}*`
         )
     }
 }
