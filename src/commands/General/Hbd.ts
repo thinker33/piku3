e0f00e864d918f91d777da62e1a777806b8711a1
bpking7 <92138643+bpking7@users.noreply.github.com>
2021-12-31 08:15:00 +0530

    Create Hbd.ts

---
+++ src/commands/General/Hbd.ts
@@ -0,0 +1,35 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "hbd",
+                        aliases: ["happy birthday", "hhp"],
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: `${client.config.prefix}hi`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"assets/hello-kitty.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `happy birthday *${this.client.config.prefix}help* list if you want anything. \n`,
+			}
+		);
+	};
+}
