175c8b21aa26163d1f01556fcb3711363acfab0b
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-02 20:10:01 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -25,8 +25,8 @@ export default class Command extends BaseCommand {
         MessageType.image,
                     undefined,
                     undefined,
-                    `_⚠️ *---------🗃️GUIDE/📄RULE ---------* ⚠️_\n\n📃Please Kindly follow the rules \n\n\n📢 use ,mods to know anything\n\n🔖 bot doesn't talk. So don't try
-To chat\n\n📌 If you want to add bot in your group the contact the mods by *,mods* \n📜Bot will not stay in less than 5 participants👀 group👋\n⚡ Don't use wrong command,\n📎use the command given in the *help list* *(,help)* \n\n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n\n⚔️ Dont Dm the bot \n-------------------------------\n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
+                    `_⚠️ *-----🗃️GUIDE/📄RULE-----* ⚠️_\n\n📃Please Kindly follow the rules \n\n\n📢 use ,mods to know anything\n\n🔖 bot doesn't talk. So don't try
+To chat\n\n📌 If you want to add bot in your group the contact the mods by *,mods* \n\n\n📜*Bot will not stay in less than 5 participants👀 group*👋\n\n⚡ Don't use wrong command,\n\n📎use the command given in the *help list* *(,help)* \n\n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n\n⚔️ Dont Dm the bot \n------------------------------------\n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
