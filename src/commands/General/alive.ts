df4ce5ae9bc9d494ce974f5bacacb0f72a777a0c
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-05 10:50:25 +0530

    Create alive.ts

---
+++ src/commands/General/alive.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "alive",
+                        aliases: ["Jinda?", "oi", "kahaho"],
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: `${client.config.prefix}alive`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `Hai, Hai \n\n I am here \n`,
+			}
+		);
+	};
+}
