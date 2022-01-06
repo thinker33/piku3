5f29e42b5e7379e730863a567d9b2a25137c7c49
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-06 17:28:51 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
             description: "Displays the Moderators' contact info",
             category: 'general',
             usage: `${client.config.prefix}mods`,
-            aliases: ['moderators', 'mod', 'owner']
+            aliases: ['moderators', 'support', 'owner']
         })
     }
 
