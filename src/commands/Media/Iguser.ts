4d15552be84dfeb18378e90d10a2fb6c58791968
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-06 09:29:39 +0530

    Update Iguser.ts

--- src/commands/Media/Iguser.ts
+++ src/commands/Media/Iguser.ts
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `🔍 *Username*: ${data.result.username}\n\n📎 *Fullname*:${data.result.full_name}\n\n✔ *Verified*:${data.result.is_verified}\n🛡️ *Private*:${data.result.is_private}\n🗃️*Postcount*:${data.result.posts_count}\n🐋 Following*:${data.result.following}\n🐣 *Followers*:${data.result.followers}\n📜*Bio*:${data.result.biography}\n`,
+                    `🔍 *Username*: ${data.result.username}\n📎 *Fullname*:${data.result.full_name}\n✔ *Verified*:${data.result.is_verified}\n🛡️ *Private*:${data.result.is_private}\n🗃️*Postcount*:${data.result.posts_count}\n🐋 Following*:${data.result.following}\n🐣 *Followers*:${data.result.followers}\n📜*Bio*:${data.result.biography}\n`,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
