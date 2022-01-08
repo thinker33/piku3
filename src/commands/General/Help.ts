98e80c71430d513e969f5dde903f9604ad38cf78
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-08 20:04:21 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,4 +1,4 @@
-7 import MessageHandler from '../../Handlers/MessageHandler'
+import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
