e8d57542d272c6095e55c4e8d86e9bc75a7b85f7
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-09 19:11:09 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -15,7 +15,7 @@ export default class EventHandler {
         )
         const groupData = await this.client.groupMetadata(event.jid);
 const members = groupData.participants.length
-if (members < 3) { 
+if (members < 5) { 
 await this.client.sendMessage(event.jid, `Bye 👋 Bye 👋 \n\n*You don't have enough member to use bot*`, MessageType.text)
 await this.client.groupLeave(event.jid)
 }
