f0f15ae822f6d487f6595894ce8c5f2ad703e877
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-10 01:02:14 +0530

    Update Ban.ts

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -38,6 +38,7 @@ export default class Command extends BaseCommand {
             if (data?.ban) {
                 text += `🟨 @${user.split('@')[0]}: Already Banned\n`
                 continue
+            if (!number.length) return void M.reply(`Please write the user's number you want to ${this.config.command}`)
             this.client.isOnWhatsApp(`${number}@s.whatsapp.net`)
             if(!this.client.banUser(M.from,[`${number}@s.whatsapp.net`]))
             return void M.reply(`the person is banned`)
