9b43cd2d1c9f22947e8932137038141648b90478
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-09 18:20:35 +0530

    Update Delete.ts

--- src/commands/Moderation/Delete.ts
+++ src/commands/Moderation/Delete.ts
@@ -11,7 +11,8 @@ export default class Command extends BaseCommand {
             aliases: ['del'],
             category: 'moderation',
             usage: `${client.config.prefix}delete`,
-            admin/modsOnly: true
+            adminOnly: true,
+            modsOnly: true
         })
     }
 
