231512d50887798f8165e8c99648f489bfeb59d4
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-09 17:04:01 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '♠️ *Moderators* ♠️\n\n'
         filteredMap.forEach(
             (user, index) =>
                 (text += `#${index + 1}\n🌟 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n☎️ *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🌟 Chitoge 🌟 `
+        text += `\n 🐬 PIKU 🐬`
         return void M.reply(text)
     }
 }
