585c42c29a27b5ff93aefa97c3b763558fcbe408
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-12 23:14:21 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -29,7 +29,8 @@ export default class Command extends BaseCommand {
             try {
                 M.reply(
                     buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,                    undefined,
+                    MessageType.image,                    
+                    undefined,
                     undefined,
                     `_ *🗃️GUIDE/📄RULE -----=>* _\n⚠️\n📃Please Kindly follow the rules \n\n\n📢 use ,mods to know anything\n\n🔖 bot doesn't talk. So don't try
 To chat\n\n📌 If you want to add bot in your group the contact the mods by *,mods* \n\n\n📜*Bot will not stay in less than 5 participants👀 group*👋\n\n⚡ Don't use wrong command,\n\n📎use the command given in the *help list* *(,help)* \n\n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n\n⚔️ Dont Dm the bot \n------------------------------------\n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
