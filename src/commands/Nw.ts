3bd8a00d7e25d980fae8983f9dacce53f7c9368f
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-13 02:25:21 +0530

    Create Nw.ts

---
+++ src/commands/Nw.ts
@@ -0,0 +1,80 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'bw',
+            description: 'Displays the help menu or shows the info of the command provided',
+            category: 'general',
+            usage: `${client.config.prefix}bw (command_name)`,
+            aliases: ['hn']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+            const n = [
+            './assets/Pikachu/images (1).mp4'
+        ]
+        let rin = n[Math.floor(Math.random() * n.length)]
+        if (!parsedArgs.joined) {
+            const commands = this.handler.commands.keys()
+            const categories: { [key: string]: ICommand[] } = {}
+            for (const command of commands) {
+                const info = this.handler.commands.get(command)
+                if (!command) continue
+                if (!info?.config?.category  || info.config.category === 'coding' || info.config.category === 'general' || info.config.category === 'weeb' || info.config.category === 'fun' || info.config.category === 'moderation' || info.config.category === 'utils' || info.config.category === 'educative' || info.config.category === 'games' || info.config.category === 'dev') continue
+                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
+                else {
+                    categories[info.config.category] = []
+                    categories[info.config.category].push(info)
+                }
+            }
+            let text = `
+╭─「⚠️WARNING - NUDITY CONTANT」
+│⋊ ᴜꜱᴇʀ: *${M.sender.username}*
+│⋊ ɴᴀᴍᴇ: *NSFW LIST*
+│⋊ ᴘʀᴇꜰɪx: ${this.client.config.prefix}
+╰────────────                            \n\n`
+            const keys = Object.keys(categories)
+            for (const key of keys)
+                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}* ${this.emojis[keys.indexOf(key)]}\n\n• \`\`\`${categories[
+                    key
+                ]
+                    .map((command) => command.config?.command)
+                    .join(' , ')}\`\`\`\n\n`
+            return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `${text} 
+ ──❅┈[ Chitoge ]┈❅───
+┌────────────┈❅
+│   🧨 PIKU
+│   ©️ Synthesized Infinity Botto
+└────────────┈⁂
+❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
+🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
+            )
+        }
+        const key = parsedArgs.joined.toLowerCase()
+        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
+        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
+        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
+        M.reply(
+            `🎈 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
+                state ? 'Disabled' : 'Available'
+            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+                command.config.aliases
+                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    : ''
+            }\n🎐 *Group Only:* ${this.client.util.capitalize(
+                JSON.stringify(!command.config.dm ?? true)
+            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+        )
+    }
+    emojis = ['👀']
+}
