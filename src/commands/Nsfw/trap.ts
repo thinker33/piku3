ca9de6609e89e5eccc717597dfaee30bd943ce32
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-13 00:58:50 +0530

    Update trap.ts

--- src/commands/Nsfw/trap.ts
+++ src/commands/Nsfw/trap.ts
@@ -17,26 +17,40 @@ export default class Command extends BaseCommand {
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		let caption = "";
-		caption += `🌟`;
+		// fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
+		const { data } = await axios.get("https://nekos.life/api/v2/img/trap");
 		if (!(await this.client.getGroupData(M.from)).nsfw)
 			return void M.reply(
 				`Don't be a pervert, Baka! This is not an NSFW group.`
 			);
-		M.reply(
-			await this.client.util.GIFBufferToVideoBuffer(
-				await this.client.getBuffer(
-					(
-						await this.client.fetch<{ url: string }>(
-							`https://api.waifu.pics/nsfw/trap`
-						)
-					).url
-				)
-			),
-			MessageType.video,
-			Mimetype.gif,
-			[caption],
-			caption
-		);
+		const buffer = await request.buffer(data.url).catch((e) => {
+			return void M.reply(e.message);
+		});
+		while (true) {
+			try {
+				M.reply(
+					buffer || "Could not fetch image. Please try again later",
+					MessageType.image,
+					undefined,
+					undefined,
+					`*Awoo...*\n`,
+					undefined
+				).catch((e) => {
+					console.log(
+						`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
+					);
+					// console.log('Failed')
+					M.reply(`Could not fetch image. Here's the URL: ${data.url}`);
+				});
+				break;
+			} catch (e) {
+				// console.log('Failed2')
+				M.reply(`Could not fetch image. Here's the URL : ${data.url}`);
+				console.log(
+					`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
+				);
+			}
+		}
+		return void null;
 	};
 }
