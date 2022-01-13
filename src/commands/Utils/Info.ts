ec524c7cf39098f01531724beddfc3077e5db407
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-13 17:36:46 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 			.all()
 			.filter((v) => !v.read_only && !v.archive)
 			.map((v) => v.jid)
-			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.map((jids) => (jids.includes("g.us") ? jids : name))
 			.filter((v) => v);
 		const pad = (s: any) => (s < 10 ? "0" : "") + s;
 		const formatTime = (seconds: any) => {
