d709fb6b6b5e58766db5ae012a059ad033d5d9a4
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-14 02:24:30 +0530

    Create Setmods.ts

---
+++ src/commands/Dev/Setmods.ts
@@ -0,0 +1,26 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'setmods',
+            description: 'Will replace the old prefix with the given term',
+            category: 'dev',
+            dm: true,
+            usage: `${client.config.prefix}setmods [new_contact]`,
+            modsOnly: true,
+            baseXp: 0
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        const addmods = this.client.config.mods.map((mod) => this.client.addContact(mod)).filter((user) => user)
+        if (!addmods) return void (await M.reply(`Please provide the new prefix.\n\n*Example: ${this.client.config.prefix}setprefix $`))
+        this.client.config.mods = addmods
+        const text = `✅ *Successfully changed the prefix to ${newprefix}.*`
+        M.reply(text)
+     }
+}
