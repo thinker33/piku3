103e03230966fd8a419570c9b8b77ef715116512
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-15 06:46:54 +0530

    Create happy_birthday.ts

--- src/commands/General/happy_birthday.ts
+++ src/commands/General/happy_birthday.ts
@@ -41,15 +41,15 @@ export default class Command extends BaseCommand {
             MessageType.image,
             undefined,
             undefined,
-            caption = "✨ツ•wíshíng чσu α vєrч vєrч•\n 🥂🎂 *HAPPY BIRTHDAY* 🎂🥂\n🥳🥳🥳✨ *@${user.split('@')[0]}*\n\n"
-         
+            `✨ツ•wíshíng чσu α vєrч vєrч•\n 🥂🎂 *HAPPY BIRTHDAY* 🎂🥂\n🥳🥳🥳✨ *@${user.split('@')[0]}*\n\n"`
+            undefined,
+            )
             return void M.reply(
             await this.client.getProfilePicture(user),
             MessageType.image,
             Mimetype.png,
             caption,
             [user]
-          )
         )
     }
 }
