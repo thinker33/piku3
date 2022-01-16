ba9674f531bdeb5ebddc21b84192b24b52729538
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-16 19:43:45 +0530

    Update dev.ts

--- src/commands/General/dev.ts
+++ src/commands/General/dev.ts
@@ -58,7 +58,7 @@ export default class Command extends BaseCommand {
 │   ©️ Synthesized Infinity Botto
 └────────────┈⁂
 ❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
-🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
+*︽]|I{•------» 𝒫𝒾𝓀𝓊 «------•}I|[︽*` }
             )
         }
         const key = parsedArgs.joined.toLowerCase()
