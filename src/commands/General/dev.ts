f967d8c592c855ed600c72fc49dcab82d83a3ff3
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-16 19:31:04 +0530

    Update dev.ts

--- src/commands/General/dev.ts
+++ src/commands/General/dev.ts
@@ -1,14 +1,14 @@
- 
- ​import​ ​{​ ​MessageType​,​ ​Mimetype​ ​}​ ​from​ ​'@adiwajshing/baileys' 
- ​import​ ​{​ ​join​ ​}​ ​from​ ​'path' 
- ​import​ ​MessageHandler​ ​from​ ​'../../Handlers/MessageHandler' 
- ​import​ ​BaseCommand​ ​from​ ​'../../lib/BaseCommand' 
- ​import​ ​WAClient​ ​from​ ​'../../lib/WAClient' 
- ​import​ ​{​ ​ISimplifiedMessage​ ​}​ ​from​ ​'../../typings' 
-  
- ​export​ ​default​ ​class​ ​Command​ ​extends​ ​BaseCommand​ ​{ 
- ​    ​constructor​(​client​: ​WAClient​,​ ​handler​: ​MessageHandler​)​ ​{ 
- ​        ​super​(​client​,​ ​handler​,​ ​{ 
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
  ​            ​command​: ​'dev'​, 
  ​            ​description​:  'command list only for mods/owners'​, 
  ​            ​category​: ​'general'​, 
