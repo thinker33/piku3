c6227ecbcc73d9d1e46c7011e51461a1bc2daaa8
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-17 20:46:04 +0530

    Update pb.ts

--- src/commands/Utils/pb.ts
+++ src/commands/Utils/pb.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "pb",
 			description: `Get text image`,
 			aliases: ["pb"],
-			category: "utils",
+			category: "creation",
 			usage: `${client.config.prefix}pb`,
 			baseXp: 50,
 		});
