de2d09569c24ef61e937373cdd96c820d13a8d65
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-17 20:42:39 +0530

    Update tneon.ts

--- src/commands/Utils/tneon.ts
+++ src/commands/Utils/tneon.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "tneon",
 			description: `Get text image`,
 			aliases: ["tneon"],
-			category: "utils",
+			category: "creation",
 			usage: `${client.config.prefix}tneon`,
 			baseXp: 50,
 		});
