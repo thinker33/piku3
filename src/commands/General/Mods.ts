927613f90f1500572a6c127a9472d1f5536ba57d
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-01-21 15:58:31 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n☎️ *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n ,.-~*¨¯¨*·~-.¸-(_ ᑭⒾⓀᑌ _)-,.-~*¨¯¨*·~-.¸`
+        text += `\n ıllıllı ᴾᴵᴷᵁ ıllıllı`
         return void M.reply(text)
     }
 }
