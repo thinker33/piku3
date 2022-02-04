bea2eb6728e0ec057658aa28dfa076400011637e
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-02-05 02:08:28 +0530

    Update Anime.ts

--- src/commands/Weeb/Anime.ts
+++ src/commands/Weeb/Anime.ts
@@ -1,89 +1,60 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import { Anime } from "mailist";
-import request from "../../lib/request";
-import { MessageType } from "@adiwajshing/baileys";
-import malScraper from "mal-scraper";
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "anime",
-			description: `Gives you the data of the given anime from MyAnimeList.`,
-			aliases: ["ani", "a"],
-			category: "weeb",
-			usage: `${client.config.prefix}anime [title]`,
-			baseXp: 50,
-		});
-	}
-
-	run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
-		if (!joined)
-			return void (await M.reply(`Give me an anime title to search, Baka!`));
-		const chitoge = joined.trim();
-		const anime = await malScraper
-			.getInfoFromName(chitoge)
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			.catch((err: any) => {
-				return void M.reply(`Couldn't find any matching anime.`);
-			});
-		const client = new Anime();
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const ani = await client.anime(chitoge).catch((err: any) => {
-			return void M.reply(`Couldn't find any matching anime.`);
-		});
-		let text = "";
-		text += `🎀 *Title: ${ani.data.anime.results[0].title.romaji}*\n`;
-		text += `🎋 *Format: ${ani.data.anime.results[0].format}*\n`;
-		text += `📈 *Status: ${ani.data.anime.results[0].status}*\n`;
-		text += `💮 *Genres: ${ani.data.anime.results[0].genres.join(", ")}*\n`;
-		text += `✨ *Based on: ${anime.source}*\n`;
-		text += `📍 *Studios: ${anime.studios.join(", ")}*\n`;
-		text += `🍥 *Producers: ${anime.producers.join(", ")}*\n`;
-		text += `🔅 *Premiered on: ${ani.data.anime.results[0].startDate.day}-${ani.data.anime.results[0].startDate.month}-${ani.data.anime.results[0].startDate.year}*\n`;
-		text += `🎐 *Season: ${ani.data.anime.results[0].season}*\n`;
-		text += `🌟 *Score: ${anime.score}*\n`;
-		text += `💎 *Rating: ${anime.rating}*\n`;
-		text += `🏅 *Rank: ${anime.ranked}*\n`;
-		text += `💫 *Popularity: ${anime.popularity}*\n`;
-		text += `🎗 *Duration: ${ani.data.anime.results[0].duration}min/episode*\n`;
-		text += `🚫 *Eechi: ${ani.data.anime.results[0].isAdult}*\n\n`;
-		text += `♦️ *Trailer: ${anime.trailer}*\n\n`;
-		text += `🌐 *URL: ${anime.url}*\n\n`;
-		text += `❄ *Description:* ${anime.synopsis}`;
-		const buffer = await request.buffer(ani.data.anime.results[0].coverImage.large).catch((e) => {
-			return void M.reply(e.message);
-		});
-		while (true) {
-			try {
-				M.reply(
-					buffer || "✖ An error occurred. Please try again later.",
-					MessageType.image,
-					undefined,
-					undefined,
-					`${text}`,
-					undefined
-				).catch((e) => {
-					console.log(
-						`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
-					);
-					// console.log('Failed')
-					M.reply(`✖ An error occurred. Please try again later.`);
-				});
-				break;
-			} catch (e) {
-				// console.log('Failed2')
-				M.reply(`✖ An error occurred. Please try again later.`);
-				console.log(
-					`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
-				);
-			}
-		}
-		return void null;
-	};
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'anime',
+            aliases: ['a', 'ani'],
+            description: 'Searches the given anime. ',
+            category: 'weeb',
+            
+            usage: `${client.config.prefix}anime [title]`
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        
+        if (!joined) return void M.reply('✖ Provide an anime to search, Baka!')
+        const chitoge = joined.trim()
+        console.log(chitoge)
+        const { data } = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${chitoge}`)
+if (!data.results[0].title) return void M.reply( await request.buffer(`https://www.linkpicture.com/q/IMG-20220118-WA0387.png`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `404 Error can not find the anime *${chitoge}*`,
+                    undefined
+                )
+        const buffer = await request.buffer(data.results[0].image_url).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || '✖ An error occurred. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `🎀 *Title: ${data.results[0].title}*\n🎋 *Ongoing: ${data.results[0].airing}*\n🎐 *Premiered on: ${data.results[0].start_date}*\n🎗 *Ended on: ${data.results[0].end_date}*\n💠 *Total Episodes: ${data.results[0].episodes}*\n🌟 *Score: ${data.results[0].score}*\n💎 *Rating: ${data.results[0].rated}*\n❄ *Description: ${data.results[0].synopsis}*\n\n🌐 *MyAnimeList URL: ${data.results[0].url}*\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`✖ An error occurred. Please try again later.`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`✖ An error occurred. Please try again later.`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
 }
