d86ee77654ca2505970f4ad5377c23658c1a36ae
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-02-06 12:11:11 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
 		const uptime = () => formatTime(process.uptime());
 		await M.reply(
 			` 🅿︎🅸︎🅺︎🆄︎ \n\n➰ *Groups:* ${
-		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length
+		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).subject
 			}\n\n♻️ *Uptime:${uptime()}*`
 		);
 	};
