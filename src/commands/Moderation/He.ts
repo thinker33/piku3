41ee4ee98489e31d36133a282c0c273ba24d750b
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-18 17:31:39 +0530

    Create He.ts

---
+++ src/commands/Moderation/He.ts
@@ -0,0 +1,37 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "evne",
+			description: "Tags all users in group chat",
+			aliases: ["alj"],
+			category: "moderation",
+			usage: `${client.config.prefix}everyone`,
+			adminOnly: true,
+			baseXp: 20,
+		});
+	}
+
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		if (!joined)
+			return void (await M.reply(`Please provide the Broadcast Message.`));
+		const term = joined.trim();
+		{
+			const text = `\n${term}\n`;{
+				caption: `${text}`,
+				contextInfo: {
+					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+				},
+			});
+		}
+	};
+}
