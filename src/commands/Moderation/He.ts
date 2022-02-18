dd24d105266a30bb303bba1ea3c527e355179b53
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-18 18:02:37 +0530

    Update He.ts

--- src/commands/Moderation/He.ts
+++ src/commands/Moderation/He.ts
@@ -23,11 +23,12 @@ export default class Command extends BaseCommand {
 		{ joined }: IParsedArgs
 	): Promise<void> => {
 		const term = joined.trim();
-		
-			const text = `\n${term}\n`;
+		{
+			const text = `\n${term}\n`,
 				caption: `${text}`,
 				contextInfo: {
 					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid)
+					}
 					}
 		}
 	
