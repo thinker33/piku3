abd00d6e7308390a689eac93bf42eeb41c71030a
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-21 19:03:51 +0530

    Create Rot.ts

---
+++ src/commands/Moderation/Rot.ts
@@ -0,0 +1,30 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'report',
+            aliases: ['rpt'],
+            category: 'general',
+            usage: `${client.config.prefix}report`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        (await this.client.mods.sendMessage(
+        M.sender.jid,
+        const term = joined.trim();
+            if (!joined)
+));
+        
+        return void this.client.mods.sendMessage(M.from, `${term}`, {quoted:M.WAMessage,
+            caption: `Sent you the report in personal message \n` }
+        )
+
+        }
+}
