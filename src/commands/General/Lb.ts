7b7dea5ae1f23af9950ae2763f09090ccc815d1f
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-24 20:41:11 +0530

    Update Lb.ts

--- src/commands/General/Lb.ts
+++ src/commands/General/Lb.ts
@@ -2,6 +2,7 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage, IUser } from "../../typings";
+import ordinal from "ordinal";
 import { MessageType } from "@adiwajshing/baileys";
 
 export default class Command extends BaseCommand {
