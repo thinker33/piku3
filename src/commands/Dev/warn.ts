ddb4dce67df2cc8c0307744d22192bce67c3a4ea
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-24 19:03:03 +0530

    Update warn.ts

--- src/commands/Dev/warn.ts
+++ src/commands/Dev/warn.ts
@@ -11,35 +11,19 @@ export default class Command extends BaseCommand {
             category: 'dev',
             usage: `${client.config.prefix}warn [@mention | tag]`,
             modsOnly: true,
-            basewarnings: 1
+            baseXp: 10
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        const immortals = this.client.config.mods
-            ? [M.sender.jid, this.client.user.jid, ...this.client.config.mods]
-            : [M.sender.jid, this.client.user.jid]
-
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-            const data = await this.client.getUser(user)
-            // const info = this.client.getContact(user)
-            // const username = info.notify || info.vname || info.name || user.split('@')[0]
-            // const username = user.split('@')[0]
-            if (data?.warnings) {
-                text += `🟨 @${user.split('@')[0]}: Already warned\n`
-                continue
-            }
-            await this.client.blockUser(user);
-            await this.client.setwarnings(user);
-            await this.client.setwarnings(M.sender.jid)
-            text += `🟥 @${user.split('@')[0]}: warned\n`
+        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
+        let username = user === M.sender.jid ? M.sender.username : 'Your'
+        if (!username) {
+            // const contact = this.client.getContact(user)
+            // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
+            username = user.split('@')[0]
         }
-        await M.reply(
-            `${text}`,
-            undefined,
-            undefined,
-            // undefined
-            [...M.mentioned, M.sender.jid]
-        )
+        return void (await M.reply(`*${username} Exp: ${(await this.client.getUser(user)).setwarnings || 0}*`))
     }
 }
