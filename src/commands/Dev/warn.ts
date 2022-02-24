45db394329497f7a8e82059e422eebd6bbab57e1
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-24 17:15:19 +0530

    Update warn.ts

--- src/commands/Dev/warn.ts
+++ src/commands/Dev/warn.ts
@@ -11,45 +11,19 @@ export default class Command extends BaseCommand {
             category: 'dev',
             usage: `${client.config.prefix}warn [@mention | tag]`,
             modsOnly: true,
-            baseXp: 0
+            basewarnings: 1
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        const immortals = this.client.config.mods
-            ? [M.sender.jid, this.client.user.jid, ...this.client.config.mods]
-            : [M.sender.jid, this.client.user.jid]
-
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
-        let text = '*STATE*\n\n'
-        // declare tagged as (string | undefined) []
-        // const tagged : (string | undefined)[] = []
-        for (const user of M.mentioned) {
-            if (immortals.includes(user)) {
-                // tagged.push(user)
-                text += `🟨 @${user.split('@')[0]} is an immortal, can't be banned\n`
-                continue
-            }
-            const data = await this.client.getUser(user)
-            // const info = this.client.getContact(user)
-            // const username = info.notify || info.vname || info.name || user.split('@')[0]
-            // const username = user.split('@')[0]
-            if (data?.warnings) {
-                text += `🟨 @${user.split('@')[0]}: done \n`
-                continue
-            }
-            await this.client.blockUser(user);
-            await this.client.warningsUser(user);
-            await this.client.warningsUser(M.sender.jid)
-            text += `🟥 @${user.split('@')[0]}: warned\n`
+        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
+        let username = user === M.sender.jid ? M.sender.username : 'Your'
+        if (!username) {
+            // const contact = this.client.getContact(user)
+            // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
+            username = user.split('@')[0]
         }
-        await M.reply(
-            `${text}`,
-            undefined,
-            undefined,
-            // undefined
-            [...M.mentioned, M.sender.jid]
-        )
+        return void (await M.reply(`*${username} Exp: ${(await this.client.getUser(user)).warnings || 0}*`))
     }
 }
