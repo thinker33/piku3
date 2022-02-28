06633a4942dcb88f135a72d2d67d69ad209e20aa
Maarz-baar <98210202+Maarz-baar@users.noreply.github.com>
2022-02-28 13:54:06 +0530

    assest fix

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -43,7 +43,8 @@ await this.client.groupLeave(event.jid)
             mentionedJid: event.actor ? [...event.participants, event.actor] : event.participants
         }
         if (add) {
-            let image = (await this.client.assets.get('0_Pokemon-2019-Nintendo-Switch-new-game-announcement-761157.jpeg'))
+            let image = this.client.assets.get('0_Pokemon-2019-Nintendo-Switch-new-game-announcement-761157.jpeg');
+	    if (typeof image === 'string') image = await request.buffer(image);
            
             if (image)
                 return void (await this.client.sendMessage(event.jid, image, MessageType.image, {
