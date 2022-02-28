641fa584ea59363096fdf8050ac1c87aa06210f8
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-28 06:41:12 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
 			.map((v) => v.jid)
 			.map((jids) => (jids.includes("g.us") ? jids : name))
 			.filter((v) => v);
- ​               ​const​ User = this.db.user.count(),
+ ​               ​const​ Users = this.db.user.count()
                 const pad = (s: any) => (s < 10 ? "0" : "") + s;
 		const formatTime = (seconds: any) => {
 			const hours = Math.floor(seconds / (60 * 60));
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
 		const uptime = () => formatTime(process.uptime());
 		await M.reply(
 			` 🅿︎🅸︎🅺︎🆄︎ \n\n➰ *Groups:* ${
-		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}\n\n\n➰ *users:* ${User.count()}\n\n♻️ *Uptime:${uptime()}*`
+		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}\n\n\n➰ *users:* ${Users.count()}\n\n♻️ *Uptime:${uptime()}*`
 		);
 	};
 }
