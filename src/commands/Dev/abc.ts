ea79dfbea5c35802b150284c67399816c36bd9fc
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-02-28 18:51:07 +0530

    Create abc.ts

---
+++ src/commands/Dev/abc.ts
@@ -0,0 +1,56 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "abc",
+			description:
+				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+			aliases: ["acast", "a.announcement", "adminbc"],
+			category: "dev",
+			dm: true,
+			usage: `${client.config.prefix}abc`,
+			modsOnly: true,
+			baseXp: 0,
+		});
+	}
+
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		if (!joined)
+			return void (await M.reply(`Please provide the Broadcast Message.`));
+		const term = joined.trim();
+		const gifs = [
+			"https://c.tenor.com/tZVpbfTIjNMAAAAC/pikachu.mp4",
+			"https://c.tenor.com/p6I6OADySl4AAAAC/piku.mp4",
+			"https://c.tenor.com/jHHKjbx6ZIoAAAAC/pikachu.mp4",
+		];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const chats: any = this.client.chats
+			.all()
+			.filter((v) => !v.read_only && !v.archive)
+			.map((v) => v.jid)
+			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.filter((v) => v);
+		for (let i = 0; i < chats.length; i++) {
+			const text = `*🍃🎋「 Admin's Broadcast 」🎋🍃*\n\n${term}\n\n`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
+				caption: `${text}`,
+				contextInfo: {
+					mentionedJid: M.groupMetadata?.admins.map((user) => user.jid),
+				},
+			});
+		}
+		await M.reply(`✅ Broadcast Message sent to *${chats.length} groups*.`);
+	};
+}
