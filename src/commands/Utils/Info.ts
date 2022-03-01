ac93d3f6884e35c3eb9477a5af16a23c291a4062
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-02 04:42:05 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -1,52 +1,63 @@
+//** @format */
+
+//** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys/lib/WAConnection";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import * as typings from "../../typings";
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
 			command: "info",
-			description: "Will display the info the bot",
-			category: "utils",
+			description: "Will display the info of the bot",
+			category: "general",
 			usage: `${client.config.prefix}info`,
 			baseXp: 0,
 		});
 	}
 
-	run = async (M: ISimplifiedMessage): Promise<void> => {
-		
+	run = async (M: typings.ISimplifiedMessage): Promise<void> => {
 		/*eslint-disable @typescript-eslint/no-explicit-any*/
-                const users = await this.client.DB.user.count();
-                const uban = await this.client.DB.user.count({ban: true});
-		const chats: any = this.client.chats
+        const chats: any = this.client.chats		
 			.all()
-			.filter((v) => !v.jid.endsWith && !v.archive)
+			.filter((v) => !v.read_only && !v.archive)
 			.map((v) => v.jid)
-			.map((jids) => (jids.includes("g.us") ? jids : name))
+			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
-                const pad = (s: any) => (s < 10 ? "0" : "") + s;
-		const formatTime = (seconds: any) => {
+		const pad = (s: any) => (s < 10 ? "0" : "") + s;
+		function formatTime(seconds: any): string {
 			const hours = Math.floor(seconds / (60 * 60));
 			const minutes = Math.floor((seconds % (60 * 60)) / 60);
 			const secs = Math.floor(seconds % 60);
 			return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
-		};
-		const uptime = () => formatTime(process.uptime());
-}
+		}
+		const users = await this.client.DB.user.count();
+                const uban = await this.client.DB.user.count({ban: true});
+		function uptime() {
+			return newFunction(formatTime);
+		}
 		this.run = async (M: typings.ISimplifiedMessage): Promise<void> => {
-		const gif = [
-			"https://c.tenor.com/tZVpbfTIjNMAAAAC/pikachu.mp4"
-		];
-		return void this.client.sendMessage(
-                             M.from,
-				{ url: gif },
+			const chitoge =
+				"https://c.tenor.com/veo9RwLpw8AAAAPo/nakano-yotsuba-wolverine.mp4";
+			return void this.client.sendMessage(
+				M.from,
+				{ url: chitoge },
 				MessageType.video,
 				{
+					quoted: M.WAMessage,
 					mimetype: Mimetype.gif,
-					caption:
-			`🅿︎🅸︎🅺︎🆄︎ \n\n➰ *Groups:* ${
-		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}\n*Ban Users: ${uban}*\n\n*Users: ${users}*\n\n♻️ *Uptime:${uptime()}*`
-		)
-	};
+					caption: `*━━━❰ 🅨︎🅞︎🅣︎🅢︎🅤︎🅑︎🅐︎ ❱━━━*\n\n🔮 *Groups: ${
+				chats.length
+					}*\n\n🚦 *Uptime: ${uptime()}*\n\n🦆 *My cute people: ${users}*\n\n\n*Ban Users: ${uban}*\n*\n\n`,
+				}
+			);
+		};
+	}
+}
+
+function newFunction(formatTime: (seconds: any) => string) {
+	return formatTime(process.uptime());
 }
