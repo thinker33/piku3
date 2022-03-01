82936d6dd71c3c7efc4f0a90fde48c4782a0db57
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-02 02:30:55 +0530

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -22,28 +22,7 @@ export default class Command extends BaseCommand {
 		M: ISimplifiedMessage,
 		{ joined }: IParsedArgs
 	): Promise<void> => {
-		const stickers = [
-			"https://wallpapercave.com/wp/wp3144753.jpg",
-			"https://wallpapercave.com/wp/wp4782018.jpg",
-			"https://wallpaperaccess.com/full/1326836.jpg",
-			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
-			"https://data.whicdn.com/images/304776416/original.jpg",
-			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
-		];
-		const option = ["--s", "--sticker"];
-		const random = stickers[Math.floor(Math.random() * stickers.length)];
-		if (!joined)
-			return void (await M.reply(
-				`${
-					M.groupMetadata?.subject || "*EVERYONE*"
-				}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
-				undefined,
-				undefined,
-				M.groupMetadata?.participants.map((user) => user.jid)
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			).catch((reason: any) =>
-				M.reply(`✖️ An error occurred, Reason: ${reason}`)
-			));
+
 		const selected = joined.trim();
 		if (!option.includes(selected))
 			return void (await M.reply(
@@ -57,17 +36,8 @@ export default class Command extends BaseCommand {
 			).catch((reason: any) =>
 				M.reply(`✖️ An error occurred, Reason: ${reason}`)
 			));
-		const sticker: any = await new Sticker(random, {
-			pack: "READ QUOTED MESSAGE",
-			author: "🌟 Chitoge 🌟",
-			quality: 90,
-			type: "full",
-			categories: ["🎊"],
-		});
-		return void (await M.reply(
-			await sticker.build(),
-			MessageType.sticker,
-			Mimetype.webp,
+			return void (await M.reply(
+			
 			M.groupMetadata?.participants.map((user) => user.jid)
 		));
 	};
