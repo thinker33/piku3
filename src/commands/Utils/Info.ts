8424f9d4c830bbcb4461fbc552ddb16503015af0
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-01 21:16:15 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		/*eslint-disable @typescript-eslint/no-explicit-any*/
                 const users = await this.client.DB.user.count();
-                const uban = await this.client.DB.ban.count();
+                const uban = await this.client.DB.banUser.count();
 		const chats: any = this.client.chats
 			.all()
 			.filter((v) => !v.jid.endsWith && !v.archive)
