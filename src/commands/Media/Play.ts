1d0970984c89b71e99279cf9368a689208c1f017
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-05 15:24:52 +0530

    Add files via upload

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
             command: 'play',
             description: '🎵 play a song with just search term!',
             category: 'media',
-            aliases: ['music', 'song'],
+            aliases: ['music'],
             usage: `${client.config.prefix}play [term]`,
             baseXp: 30
         })
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('⏳ Sending...')
+        M.reply('🌟 Sending...')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
--- src/commands/Media/YTVideo.ts
+++ src/commands/Media/YTVideo.ts
@@ -3,7 +3,9 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import YT from '../../lib/YT'
-import { ISimplifiedMessage } from '../../typings'
+const ytdl = require('ytdl-core');
+const Innertube = require('youtubei.js');
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -17,16 +19,30 @@ export default class Command extends BaseCommand {
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!M.urls.length) return void M.reply('🔎 Provide the URL of the YT video you want to download')
+    run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+        if (!M.urls.length) return void M.reply('ðŸ”Ž Provide the URL of the YT video you want to download')
+        const youtube = await new Innertube(); 
         const video = new YT(M.urls[0], 'video')
+        const te = joined.trim();
+        let id = await ytdl.getURLVideoID(te)
+        const v = await youtube.getDetails(id);
+        let text = `*Title*: ${v.title}
+*Channel*: ${v.metadata.channel_name}
+*Likes*: ${v.metadata.likes}
+*Dislike*: ${v.metadata.dislikes}
+*Views*: ${v.metadata.view_count}
+*Duration* (sec): ${v.metadata.length_seconds}
+*Description*: ${v.description}`
         if (!video.validateURL()) return void M.reply(`Provide a Valid YT URL`)
         const { videoDetails } = await video.getInfo()
-        M.reply('🌟 Sending...')
+        M.reply('Sending...')
         if (Number(videoDetails.lengthSeconds) > 1800)
-            return void M.reply('⚓ Cannot download videos longer than 30 minutes')
-        M.reply(await video.getBuffer(), MessageType.video).catch((reason: Error) =>
-            M.reply(`✖ An error occurred, Reason: ${reason}`)
+            return void M.reply('Cannot download videos longer than 30 minutes')
+        M.reply(await video.getBuffer(), MessageType.video, undefined, undefined, text, undefined).catch((reason: Error) =>
+            M.reply(`An error occurred, Reason: ${reason}`)
         )
     }
 }
