87e3c3cf7e6a0f62ffdc678fce79a7496e74bdcc
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-06 20:33:09 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -15,6 +15,7 @@ export default class Command extends BaseCommand {
             aliases: ['rules', 'guide', 'bot'],
             category: 'general',
             usage: `${client.config.prefix}rules`,
+            dm: true,
             baseXp: 5000
         })
     }
@@ -25,7 +26,7 @@ export default class Command extends BaseCommand {
                     undefined,
                     undefined,
                     `_ *🗃️GUIDE/📄RULE -----=>* _\n⚠️\n📃Please Kindly follow the rules \n\n\n📢 use ,mods to know anything\n\n🔖 bot doesn't talk. So don't try
-To chat\n\n📌 If you want to add bot in your group the contact the mods by *,mods* \n\n\n📜 *Bot will not stay in less than 5 participants👀 group* 👋\n\n⚡ Don't use wrong command,\n\n📎use the command given in the *help list* *(,help)* \n\n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n\n⚔️ Dont Dm the bot \n------------------------------------\n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
+To chat\n\n📌 If you want to add bot in your group the contact the mods by *,mods* \n\n\n📜 *Bot will not stay in less than 5 participants👀 group* 👋\n\n🔹 Use report command if you have complaints, issue, suggestions.\n🔺 You will be ban after 3 warning. So don't Spam.\n\n⚡ Don't use wrong command,\n\n📎use the command given in the *help list* *(,help)* \n\n⚠️ Dont spam the bot with commands if the bot is not responding, It means the bot maybe offline or facing internet issue. \n\n⚔️ Dont Dm the bot \n------------------------------------\n\n💣IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
