12f954d4c586d558146de6384a829a03d4ab197c
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-13 12:46:06 +0530

    Create Nocmd.ts

---
+++ src/commands/Moderation/Nocmd.ts
@@ -0,0 +1,26 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/my_love_pikachu2-20220304-0002 (1).mp4'
+        ]
+        let lucy = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: lucy }, MessageType.video, {
+            quoted: M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `No such command ${M.sender.username}! Do you mean *${this.client.config.prefix}help*? \n` }
+        )
+    }
+}
