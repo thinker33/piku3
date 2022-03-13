7b12f9254a54c31b0a572d2cc02c6b9a95f4a560
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-13 13:01:42 +0530

    Update Admins.ts

--- src/commands/General/Admins.ts
+++ src/commands/General/Admins.ts
@@ -14,14 +14,15 @@ export default class Command extends BaseCommand {
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
+    run = async (M: ISimplifiedMessage{ joined }): Promise<void> => {
+      const tem = joined.trim();
        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
         let username = user === M.sender.jid ? M.sender.username : ''
         if (!username) {
             const contact = this.client.getContact(user)
             username = contact.notify || contact.vname || contact.name || user.split('@')[0]
         }
-        return void (await M.reply(`*ADMINS!*\n\n[ You are summoned by *@${user.split('@')[0]}* ]`, undefined, undefined, M.groupMetadata?.admins).catch(
+        return void (await M.reply(`*ADMINS!*\n\n[ `${tem}` by *@${user.split('@')[0]}* ]`, undefined, undefined, M.groupMetadata?.admins).catch(
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             (reason: any) => M.reply(`an error occurred, Reason: ${reason}`)
         ))
