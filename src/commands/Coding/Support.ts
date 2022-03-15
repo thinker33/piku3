f797e8699ac02f0ac30f992ad327545d5f4ef9fa
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-16 00:37:47 +0530

    Create Support.ts

---
+++ src/commands/Coding/Support.ts
@@ -0,0 +1,35 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'support',
+            aliases: ['support'],
+            description: 'Gets the support group links',
+            category: 'framework',
+            usage: `${client.config.prefix}Support`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        (await this.client.sendMessage(
+        M.sender.jid,
+        ` *Gambling bot* \n\n https://chat.whatsapp.com/ChvkiST07BKGSgjX71nWQO`,
+           MessageType.text
+        ))
+        const n = [
+            './assets/my_love_pikachu2-20220304-0002 (1).mp4'
+        ]
+        let beckylynch = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `Sent you the support Link in personal message \n` }
+        )
+
+        }
+}
