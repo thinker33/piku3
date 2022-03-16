41698649aacd61a41827d9769bee2d463f7f4b70
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-16 22:42:08 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -93,10 +93,14 @@ export default class MessageHandler {
 				)}`
 			);
 			if (!command)
-				return void M.reply(
-					`*꧁ Hello there ꧂* \n\n Thanks for using bot. Bot is available for you kindly use *${this.client.config.prefix}help* .`
-				);
-			const user = await this.client.getUser(M.sender.jid);
+				return void M.reply( await request.buffer(`https://lh3.googleusercontent.com/pw/AM-JKLXDWWrhHtuvfs9E-inra71tvu8fsRB2LYhFcnn57Jr49Ja0T-24jo9nCpRu8uq5goCiSum2m33vmNczqCsyuCfc_5rY1wjL_LV8Tmer0q3D4Zs-pfJDmvvn2_R0iETsdl5D7iVx-JALFur3riS8xoH_=w1440-h810-no?authuser=0`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `No such command, Baka! Have you never seen someone use the command *${this.client.config.prefix}help*`,
+                    undefined
+                )
+		const user = await this.client.getUser(M.sender.jid);
 			if (user.ban) return void M.reply("You're Banned from using commands.\n Talk to mods ┐(￣ヘ￣)┌....");
 			const state = await this.client.DB.disabledcommands.findOne({
 				command: command.config.command,
