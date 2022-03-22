151797e42a7366772e82c4658bc77ad3383df6eb
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-22 18:15:04 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -58,7 +58,7 @@ export default class Command extends BaseCommand {
 │   ©️ Synthesized Infinity Botto
 └────────────┈⁂
 ❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
-🐇 *USER Exp*: *${(await this.client.getUser(M.sender)).Xp || 0}*  ` }
+🐇 *USER Exp*: *${(await this.client.getUser(M.sender.jid)).Xp || 0}*  ` }
             )
         }
         const key = parsedArgs.joined.toLowerCase()
