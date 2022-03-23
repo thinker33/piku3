28abba9ba5afcf318b9c8959f8a0bd1d70bc60c0
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-23 11:31:32 +0530

    Update Textmaker.ts

--- src/commands/Coding/Textmaker.ts
+++ src/commands/Coding/Textmaker.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
          const n = [
-            './assets/Pikachu/Pikachu.mp4'
+            './assets/Pikachu/ezgif-2-f9c4b6f8f1.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
