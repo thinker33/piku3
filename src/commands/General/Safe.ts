cf0735cd1ba17a3c017fcf98d433cc0eeb8f8002
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-26 00:53:11 +0530

    Create Safe.ts

---
+++ src/commands/General/Safe.ts
@@ -0,0 +1,27 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+
+            aliases: ['fick'],
+            command: 'testm',
+            description: 'removes the mentioned users',
+            category: 'moderation',
+            usage: `testm`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        let text = '*Action*\n\n' else {
+                text += `🟥 Removed *@${user.split('@')[0]}*\n`
+                await this.client.groupRemove(M.from, [user])
+            }
+        })
+        await M.reply(`${text}`, undefined, undefined, [ M.sender.jid])
+    }
+}
