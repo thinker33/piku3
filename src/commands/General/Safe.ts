56ebfdf65e0a2889302291cd00b3fd188537eba8
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-26 15:38:58 +0530

    Update Safe.ts

--- src/commands/General/Safe.ts
+++ src/commands/General/Safe.ts
@@ -18,9 +18,9 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        {const user: M.sender.jid
-        }
+        if (M.quoted?.sender) 
+        const user: M.sender.jid
+        
         await this.client.groupRemove(M.from, [user])
     }
 }
