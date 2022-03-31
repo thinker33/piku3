12b132be0e83fd74483eebb21b602ebe6265de6d
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-03-31 08:48:36 +0530

    Update happy_birthday.ts

--- src/commands/General/happy_birthday.ts
+++ src/commands/General/happy_birthday.ts
@@ -1,7 +1,6 @@
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
