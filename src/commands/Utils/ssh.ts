1dc4206dd7e399ae1ee9dce6afe6f304604e6fd8
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-04-07 02:20:57 +0530

    Create ssh.ts

---
+++ src/commands/Utils/ssh.ts
@@ -0,0 +1,52 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import { Sticker, Categories, StickerTypes } from 'wa-sticker-formatter'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'stickerserach',
+            aliases: ['ssh', 'ssc'],
+            description: 'Will Sent A Random Sticker. ',
+            category: 'utils',
+            dm: false,
+            usage: `${client.config.prefix}stickerserach [keywords]`
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+
+        if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
+        const cara = joined.trim()
+        console.log(cara)
+
+        const { data } = await axios.get(`https://g.tenor.com/v1/search?q=${cara}&key=LIVDSRZULELA&limit=8`)
+
+if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
+        //const i = Math.floor(Math.random() * data.result.length)
+const b = `${data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.mp4?.url}`
+
+        const sticker: any = await new Sticker(b, {
+			pack: "sticker",
+			author: "PIKU 🐇",
+			quality: 40,
+			type: "crop",
+			categories: ["🥂"],
+		});
+
+      await M.reply(
+			await sticker.build(),
+			MessageType.sticker,
+			Mimetype.webp,)
+}
+
+
+
+
+
+}
